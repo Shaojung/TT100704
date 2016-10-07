9a8acae4e49c1051f655094a5e96f203625305f2
Shaojung <shaojung@gmail.com>
2016-10-07 15:22:15 +0800

    Use String[]

--- app/src/main/java/com/test/tt100704/MainActivity.java
+++ app/src/main/java/com/test/tt100704/MainActivity.java
@@ -2,12 +2,24 @@ package com.test.tt100704;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+
+        String[] fruits = {"蘋果", "檸檬", "香蕉", "芭樂"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                        MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        fruits
+        );
+        lv.setAdapter(adapter);
+
     }
 }
