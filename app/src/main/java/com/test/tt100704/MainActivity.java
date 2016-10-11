b1b41f95dea5fea8ec1f974686f5ff1c29dcd15e
Shaojung <shaojung@gmail.com>
2016-10-11 09:27:34 +0800

    Add ImageView

--- app/src/main/java/com/test/tt100704/MainActivity.java
+++ app/src/main/java/com/test/tt100704/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-    ArrayList<Map<String, String>> mylist;
+    ArrayList<Map<String, Object>> mylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +22,27 @@ public class MainActivity extends AppCompatActivity {
         Map m1 = new HashMap();
         m1.put("city", "台北");
         m1.put("code", "02");
+        m1.put("img", R.drawable.c1);
         mylist.add(m1);
 
         Map m2 = new HashMap();
         m2.put("city", "台中");
         m2.put("code", "04");
+        m1.put("img", R.drawable.c2);
         mylist.add(m2);
 
         Map m3 = new HashMap();
         m3.put("city", "台南");
         m3.put("code", "06");
+        m1.put("img", R.drawable.c3);
         mylist.add(m3);
 
         SimpleAdapter adapter = new SimpleAdapter(
                 MainActivity.this,
                 mylist,
                 R.layout.myitem,
-                new String[] {"city", "code"},
-                new int[] {R.id.textView, R.id.textView2}
+                new String[] {"city", "code", "img"},
+                new int[] {R.id.textView, R.id.textView2, R.id.imageView}
 
         );
 
